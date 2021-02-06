228bad8ee52ddc9bf9a6ebf735f1f082666961d0
Alexander Polyakov <devops.edc@gmail.com>
2021-02-06 17:57:30 +0300

    Change ports for docker container

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ apt update -y
 apt install docker.io -y
 systemctl enable --now docker
 docker build -t boxfuse-docker:latest .
-docker run -d -it -p 80:80 boxfuse-docker
+docker run -d -it -p 8080:8080 boxfuse-docker
