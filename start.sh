d4582cbdb058e187e904758799019d1e4bc25736
Alexander Polyakov <devops.edc@gmail.com>
2021-02-07 16:49:33 +0300

    Remove autodelete containers and images from start.sh, add instruction to Readme.md

--- start.sh
+++ start.sh
@@ -3,7 +3,5 @@
 apt update -y
 apt install docker.io -y
 systemctl enable --now docker
-docker rm $(docker ps -aq) -f
-docker images prune
 docker build -t boxfuse-docker:latest .
 docker run -d -it -p 8080:8080 boxfuse-docker
